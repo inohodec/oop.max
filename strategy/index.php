f0f006ea3ba9729d50480af1a5fa75b791c5e210
inohodec <judas.box@gmail.com>
2018-01-09 22:42:55 +0200

    My first Commit

--- strategy/index.php
+++ strategy/index.php
@@ -1,8 +1,27 @@
 <?php
-class Duck {
+interface SuperDuck {
+
+}
+class Duck implements SuperDuck {
     public $color;
 
     public function __construct()
     {
+    }
+}
+class SmallDuck extends Duck {
+    public $size;
+    function fly(Features $flying) {
+        $flying->flying();
+    }
+}
+
+interface Features {
+    public function flying();
+}
+class RocketFly implements Features {
+    function flying()
+    {
+        // TODO: Implement flying() method.
     }
 }
